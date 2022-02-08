ff4349b75812a36db7d7b450d36b84796abde1a2
Esau Matias <esaufreirematias@gmail.com>
2022-02-08 18:27:48 -0300

    resolve conflitos

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -34,7 +34,7 @@ function Login() {
           </Form.Group>
           <Form.Control
             variant="primary"
-            type="submit"
+            type="button"
             onClick={setUser}
             className="inputButton"
             value="START"
